4235d82a7a0523eafb810f87a522e6756b1eed63
nathaly <nathy_peretto@hotmail.com>
2022-01-30 19:20:03 -0300

    Teste pagina Senai

---
+++ HomePage.java
@@ -0,0 +1,43 @@
+package br.senai.sp.homesenai;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HomePage {
+		private WebDriver driver;
+		
+		@Before
+		public void Setup() {
+			System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		}
+		
+		@Test
+		public void TesteNavegarParaGoogle() {
+			driver.get("https://online.sp.senai.br");
+			
+			WebElement input = driver.findElement(By.id("Busca1_txtFiltro"));
+			
+			input.sendKeys("cursos");
+			input.sendKeys(Keys.ENTER);
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.findElement(By.xpath("//*[@id=\"MainContent_ListaCurso1_rptSegmArea_rptCursos_0_divCurso_0\"]/a")).click();
+			
+			driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("cursos");
+			driver.findElement(By.id("Busca1_btnBusca")).click();
+		}
+			@After 
+			public void FecharNavegador() {
+				driver.close();
+}
+}
